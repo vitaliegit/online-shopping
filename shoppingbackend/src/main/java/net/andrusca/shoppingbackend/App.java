4a17357c50d08d206a5ee5028c7fe3766bfbf242
VitalieGit <vitaliegit@yandex.ru>
2018-11-02 15:08:29 +0200

    Project setup completede succesefuly

---
+++ shoppingbackend/src/main/java/net/andrusca/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.andrusca.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
